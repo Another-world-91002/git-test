ca693556b891b664025620537407810c1ee15751
yueya <2844573252@qq.com>
2022-10-09 21:23:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author yywstart
+ * @date 2022/10/9 - 21:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
